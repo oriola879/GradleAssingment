220484f4421fd96161595b99dde5e3d3dd3cdbbc
Oriola <oriola.arifi24@gmail.com>
2024-12-04 20:33:53 +0100

    Initial commit

---
+++ src/main/java/com/example/my_gradle_project/MyGradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_gradle_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGradleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGradleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_gradle_project/MyGradleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_gradle_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGradleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
